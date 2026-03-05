a865e863570679f2b5cc5f34de485ba313a860b9
Jasem <2961000A@student.gla.ac.uk>
2026-03-05 21:08:53 +0000

    Added views, URLs, and templates for home, play, leaderboard, history, and result pages

---
+++ game/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('play/', views.play, name='play'),
+    path('leaderboard/', views.leaderboard, name='leaderboard'),
+    path('history/', views.history, name='history'),
+]
--- game/views.py
+++ game/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+
+    context = {}
+
+    return render(request, "game/home.html", context)
+
+def play(request):
+
+    context = {}
+
+    return render(request, "game/play.html", context)
+
+def leaderboard(request):
+
+    context = {}
+
+    return render(request, "game/leaderboard.html", context)
+
+def history(request):
+
+    context = {}
+
+    return render(request, "game/history.html", context)
