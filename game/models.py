7db4a69668cf2a9c893c4202815875bd0320a811
Zoe Baker <2975768B@student.gla.ac.uk>
2026-03-06 15:51:47 +0000

    some models added: Artist, Artwork, Country, Region, Continent

--- game/models.py
+++ game/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Artist(models.Model):
+    id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=50)
+
+class Continent(models.Model):
+    id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=50)
+
+class Region(models.Model):
+    id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=50)
+    continent = models.ForeignKey(Continent)
+
+class Country(models.Model):
+    id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=50, unique=True)
+    region = models.ForeignKey(Region)
+
+class Artwork(models.Model):
+    id = models.IntegerField(unique=True)
+    title = models.CharField(max_length=128)
+    artist = models.ForeignKey(Artist)
+    country = models.ForeignKey(Country)
+    year = models.IntegerField()
