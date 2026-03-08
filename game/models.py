9de1a69be14eab3eaaea642614138871ae3b46fe
Haoyu Wang <3075329W@student.gla.ac.uk>
2026-03-08 13:38:17 +0000

    Refactor models
    - remove manual id fields
    - add on_delete to ForeignKeys
    - add image_url field to Artwork

--- game/models.py
+++ game/models.py
@@ -1,28 +1,60 @@
 from django.db import models
 
-# Create your models here.
 
 class Artist(models.Model):
-    id = models.IntegerField(unique=True)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
 
 class Continent(models.Model):
-    id = models.IntegerField(unique=True)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
 
 class Region(models.Model):
-    id = models.IntegerField(unique=True)
     name = models.CharField(max_length=50)
-    continent = models.ForeignKey(Continent)
+    continent = models.ForeignKey(
+        Continent,
+        on_delete=models.CASCADE,
+        related_name="regions"
+    )
+
+    def __str__(self):
+        return self.name
+
 
 class Country(models.Model):
-    id = models.IntegerField(unique=True)
     name = models.CharField(max_length=50, unique=True)
-    region = models.ForeignKey(Region)
+    region = models.ForeignKey(
+        Region,
+        on_delete=models.CASCADE,
+        related_name="countries"
+    )
+
+    def __str__(self):
+        return self.name
+
 
 class Artwork(models.Model):
-    id = models.IntegerField(unique=True)
     title = models.CharField(max_length=128)
-    artist = models.ForeignKey(Artist)
-    country = models.ForeignKey(Country)
+    artist = models.ForeignKey(
+        Artist,
+        on_delete=models.CASCADE,
+        related_name="artworks"
+    )
+    country = models.ForeignKey(
+        Country,
+        on_delete=models.CASCADE,
+        related_name="artworks"
+    )
     year = models.IntegerField()
+    image_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+
