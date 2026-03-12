a8d89e8a9fe61a0799d51566a9f9dbf62e7762d2
kailin lo <3012337l@student.gla.ac.uk>
2026-03-12 18:31:57 +0000

    urls + views skeleton

--- game/urls.py
+++ game/urls.py
@@ -3,7 +3,20 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('play/', views.play, name='play'),
-    path('leaderboard/', views.leaderboard, name='leaderboard'),
-    path('history/', views.history, name='history'),
+
+    path(r'login/$', views.login, name='login'),
+    path(r'^register/$', views.register, name='register'),
+    path(r'^logout/$', views.logout, name='logout'),
+
+
+    path(r'play/$', views.play, name='play'),
+    path(r'^result/$', views.result, name='result'),
+    path(r'^artwork/$', views.artwork, name='artwork'),
+
+
+    path(r'leaderboard/$', views.leaderboard, name='leaderboard'),
+
+
+    path(r'history/$', views.history, name='history'),
+    path(r'settings/$', views.settings, name='settings'),
 ]
--- game/views.py
+++ game/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -8,11 +9,53 @@ def home(request):
 
     return render(request, "game/home.html", context)
 
+
+
+
+def login(request):
+
+    context = {}
+
+    return render(request, "game/login.html", context)
+
+
+def register(request):
+
+    context = {}
+
+    return render(request, "game/register.html", context)
+
+
+def logout(request):
+
+    context = {}
+
+    return render(request, "game/logout.html", context)
+
+
+
+
+
 def play(request):
 
     context = {}
 
     return render(request, "game/play.html", context)
+
+def result(request):
+
+    context = {}
+
+    return render(request, "game/result.html", context)
+
+def artwork(request):
+
+    context = {}
+
+    return render(request, "game/artwork.html", context)
+
+
+    
 
 def leaderboard(request):
 
