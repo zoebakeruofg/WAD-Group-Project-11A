92310d402da398670aa5f2b48ba1e03a946ddf5d
Haoyu Wang <3075329W@student.gla.ac.uk>
2026-03-13 15:19:46 +0000

    Add GameSession to store the history

--- game/models.py
+++ game/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Artist(models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -57,4 +57,27 @@ class Artwork(models.Model):
     def __str__(self):
         return self.title
 
+class GameSession(models.Model):
 
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="sessions"
+    )
+
+    artwork = models.ForeignKey(
+        Artwork,
+        on_delete=models.CASCADE
+    )
+
+    guess_continent = models.CharField(max_length=50, blank=True)
+    guess_country = models.CharField(max_length=50, blank=True)
+    guess_artist = models.CharField(max_length=50, blank=True)
+    guess_year = models.IntegerField(null=True, blank=True)
+
+    score = models.IntegerField(default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.artwork.title}"
