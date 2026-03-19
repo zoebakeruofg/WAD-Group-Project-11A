8f5cb5c6d5d684de526bc57b4e5318f522133233
kailin lo <3012337l@student.gla.ac.uk>
2026-03-19 20:49:34 +0000

    added authentication, form response collection and updated register function

--- game/views.py
+++ game/views.py
@@ -1,15 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Continent
-
-def get_server_side_cookie(request, cookie, default_val=None):
-    val = request.session.get(cookie)
-    if not val:
-        val = default_val
-    return val
-
-
-
+from .models import Artwork, Artist, Country
+from .models import Artwork, GameSession, AdminProfile
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout, authenticate, login
+from django.views.decorators.http import require_POST
 
 
 def home(request):
@@ -21,6 +16,16 @@ def home(request):
 
 
 def login(request):
+    if request.method == "POST":
+
+        username = request.POST.get("username", "").strip()
+        password = request.POST.get("password", "")
+
+        authority_type = request.POST.get("role", "user").strip().lower()
+
+        matriculation_num = request.POST.get("guid", "").strip().upper()
+
+
     return render(request, "game/login.html")
 
 
@@ -29,8 +34,63 @@ def logout(request):
     return redirect("home")
 
 
+ADMIN_NUMS = {"3010809L", "3075329W", "3012337L", "2961000A", "2975768B",}
+
 def register(request):
-    return render(request, "game/register.html")
+
+    if request.method == "POST":
+
+        email = request.POST.get("email", "").strip()
+
+        username = request.POST.get("username", "").strip()
+
+        password = request.POST.get("password", "")
+        confirm_password = request.POST.get("confirm_password", "")
+        
+        acc_type = request.POST.get("acc_type", "user").strip().lower()
+        matriculation_num = request.POST.get("guid", "").strip().upper()
+
+    if not email or not username or not password or not confirm_password:
+        return render(
+            request, 
+            "game/create_account.html", 
+            {"error": "Form error: ensure that all fields have been filled in"})
+
+    if password != confirm_password:
+        return render(
+            request, 
+            "game/create_account.html", 
+            {"error": "Password error: passwords do not match"})
+
+    if User.objects.filter(username=username).exists():
+        return render(request,
+        "game/create_account.html",
+        {"error": "This username has already been taken, please try another one."})
+
+    if User.objects.filter(email=email).exists():
+        return render(request, "game/create_account.html", {"error": "An account with this email already exists, please go to login"})
+
+    user_is_admin = False
+    if acc_type == "admin":
+        user_is_admin = True
+        if matriculation_num not in ADMIN_NUMS:
+            return render(request, "game/create_account.html", {"error" :"matriculation number invalid, only authenticated GUIDs can create an admin account"})
+        
+    user = User.objects.create_user(username=username, email=email, password=password)
+
+    if user_is_admin:
+        user.is_staff = True
+        user.save()
+        
+        AdminProfile.objects.create(
+            user=user,
+            guid=guid
+        )
+
+    login(request, user)
+    return redirect("home")
+
+    return render(request, "game/create_account.html")
 
 
 
@@ -53,6 +113,15 @@ def result(request):
 
 @login_required(login_url='login')
 def make_guess(request):
+
+    continent_guess = request.POST.get("continent", "").strip().lower()
+    country_guess = request.POST.get("country", "").strip().lower()
+
+    artist_guess = request.POST.get("artist", "").strip().lower()
+
+    year_guess = request.POST.get("year", "").strip().lower()
+
+
     return render(request, "game/artwork_information.html")
 
 @login_required(login_url='login')
@@ -75,22 +144,22 @@ def settings(request):
 
 
 
-
+@staff_member_required(login_url='login')
 def manage_users(request):
     return render(request, "game/manage_users.html")
 
-
+@staff_member_required(login_url='login')
 def manage_artworks(request):
     return render(request, "game/manage_artworks.html")
 
 
 
 
-
+@staff_member_required(login_url='login')
 def enable_user(request):
     return render(request)
 
-
+@staff_member_required(login_url='login')
 def disable_user(request):
     return render(request)
 
